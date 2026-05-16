fe0fd323abfd9a6ca7e911cdc963e7f46caee81a
aletheiaops <xaletheiax@protonmail.com>
2026-05-15 21:28:02 -0400

    Encode agent name and order UUID in reminder email

--- common/mail/order-reminder-html.php
+++ common/mail/order-reminder-html.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 /* @var $order common\models\Order */
 ?>
 <div class="verify-form">
-    <h1>Dear <?= $agent_name ?>,</h1>
+    <h1>Dear <?= Html::encode($agent_name) ?>,</h1>
 
     <p class="lead">
-      We would like to remind you that Order #<?= $order->order_uuid ?> has been placed 5 minutes ago for total amount of
+      We would like to remind you that Order #<?= Html::encode($order->order_uuid) ?> has been placed 5 minutes ago for total amount of
         <?= \Yii::$app->formatter->asCurrency($order->total, $order->currency->code, [\NumberFormatter::MAX_SIGNIFICANT_DIGITS => $order->currency->decimal_place]) ?>
           <br/>
           <br/>
